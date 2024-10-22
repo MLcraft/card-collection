60fbe851ac6966d33ae8d95ef34a5b334868cb44
MLcraft <michelleliu.ca@gmail.com>
2024-10-22 02:00:30 +0000

    get user by discord endpoint

--- src/main/java/com/shizubro/cardcollection/controller/UserInfoController.java
+++ src/main/java/com/shizubro/cardcollection/controller/UserInfoController.java
@@ -2,11 +2,9 @@ package com.shizubro.cardcollection.controller;
 
 import com.shizubro.cardcollection.service.UserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.UUID;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
 public class UserInfoController {
